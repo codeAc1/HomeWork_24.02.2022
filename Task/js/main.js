e9743fbec5a84dfa32120eeb691670ecec0b9352
Sakhavat <sakhavatra@code.edu.az>
2022-02-24 12:23:27 +0400

    todolist ready

--- Task/js/main.js
+++ Task/js/main.js
@@ -1,48 +1,50 @@
 
-// inputun uzerinde  olanda Ender duymesini basanda button click olsun
+// inputun uzerinde  olanda Enter duymesini basanda button click olsun
 var inp_enter = document.querySelector("#First .center #inp_text");
-inp_enter.addEventListener("keyup", function(e) {
-    
-    if (e.keyCode === 13) {
-      e.preventDefault();
-      document.querySelector("#First .center #btn_add").click();
-    }
-  });
+inp_enter.addEventListener("keyup", function (e) {
 
- //Click Edende inport etsin
-let Add=document.querySelector("#First .center #btn_add")
-Add.addEventListener("click",function()
-{
-    
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        document.querySelector("#First .center #btn_add").click();
+    }
+});
+
+//Click Edende inport etsin
+let Add = document.querySelector("#First .center #btn_add")
+Add.addEventListener("click", function () {
+
     var list_item = document.querySelector("#First .center #inp_text").value;
     if (!list_item.trim(' ')) {
         alert("boş olmaz")
     } else {
         var list = document.createElement("li");
-        var btn =document.createElement("button")
+        var btn = document.createElement("button")
         const listElement = document.querySelector('ul')
         list.innerText = list_item;
-        btn.innerText="X"
-        list.setAttribute("class","list-group-item list-group-item-secondary my-1")
-        btn.setAttribute("class","btn btn-danger")
+        btn.innerText = "X"
+        list.className="list-group-item list-group-item-secondary my-1"
+        btn.className= "btn btn-danger"
+        btn.onclick=function(){
+            this.parentElement.remove();
+        }
         document.getElementById("list_ul").appendChild(list);
         let all_dell_btn = document.querySelectorAll("#list_ul li")
-        all_dell_btn.forEach(all_dell_btn => { all_dell_btn.appendChild(btn)})
-        list_item = document.querySelector("#First .center #inp_text").value="" 
-        document.querySelector("#First .center #inp_text").focus() 
+        all_dell_btn.forEach(all_dell_btn => { all_dell_btn.appendChild(btn) })
+        list_item = document.querySelector("#First .center #inp_text").value = ""
+        document.querySelector("#First .center #inp_text").focus()
     }
-    
+
 })
 
 //Butun siyahini silir
-let Dell_All=document.querySelector("#First #btn_all_delete")
-Dell_All.addEventListener("click",function()
-{
+let Dell_All = document.querySelector("#First #btn_all_delete")
+Dell_All.addEventListener("click", function () {
     let dell_all = document.querySelectorAll("#list_ul li")
     dell_all.forEach(function (item) {
-            item.remove();
+        item.remove();
     });
 })
 
-//Siyahidaki list elementlerini tek tek silir
+
+
 
